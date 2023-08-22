59eb775f440bbf278b850886b333ba8c74897cb4
Patrick Willian <patrickwcg@gmail.com>
2023-08-21 22:18:42 -0300

    first commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,47 @@
+import React, {useContext} from 'react';
+import {Text, View} from 'react-native';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import HomeScreen from '../screens/HomeScreen';
+import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+import {AuthContext} from '../context/AuthContext';
+import SplashScreen from '../screens/SplashScreen';
+
+const Stack = createStackNavigator();
+
+const Navigation = () => {
+  const {userInfo, splashLoading} = useContext(AuthContext);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {splashLoading ? (
+          <Stack.Screen
+            name="Splash Screen"
+            component={SplashScreen}
+            options={{headerShown: false}}
+          />
+        ) : userInfo.access_token ? (
+          <Stack.Screen name="Home" component={HomeScreen} />
+        ) : (
+          <>
+            <Stack.Screen
+              name="Login"
+              component={LoginScreen}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="Register"
+              component={RegisterScreen}
+              options={{headerShown: false}}
+            />
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
